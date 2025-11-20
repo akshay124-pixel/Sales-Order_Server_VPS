16960563c27b2f97275368945c27de2550524af7
Akshay Pratap Singh <akshaypratapsingh1244@gmail.com>
2025-11-20 13:00:36 +0530

    Refine CORS config comment for clarity

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Controller = require("./Controller/Logic");
 const app = express();
 const server = http.createServer(app);
 
-// ✅ CORS config check it
+// ✅ CORS config 
 const corsOptions = {
   origin: [
     "https://sales-order-app-eight.vercel.app", // frontend vercel
